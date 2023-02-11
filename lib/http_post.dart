4858f84eec2ecad7309c5883e00898274dfe163e
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-02-11 08:13:26 +0700

    added http put

--- lib/http_post.dart
+++ lib/http_post.dart
@@ -46,23 +46,69 @@ class _HttpPostState extends State<HttpPost> {
             ),
           ),
           const SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: () async {
-              var response = await http.post(
-                Uri.parse("https://reqres.in/api/users"),
-                body: {
-                  "name": namaC.text,
-                  "job": jobC.text,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  if (namaC.text == "" && jobC.text == "") {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("Isi form data!!")));
+                  } else {
+                    var responsePost = await http.post(
+                      Uri.parse("https://reqres.in/api/users"),
+                      body: {
+                        "name": namaC.text,
+                        "job": jobC.text,
+                      },
+                    );
+                    Map<String, dynamic> dataResponPost =
+                        json.decode(responsePost.body) as Map<String, dynamic>;
+                    // print(dataRespon["name"]);
+                    setState(() {
+                      hasilResponse =
+                          "${dataResponPost['name']} - ${dataResponPost['job']} [POST]";
+                    });
+                  }
                 },
-              );
-              Map<String, dynamic> dataRespon =
-                  json.decode(response.body) as Map<String, dynamic>;
-              // print(dataRespon["name"]);
-              setState(() {
-                hasilResponse = "${dataRespon['name']} - ${dataRespon['job']}";
-              });
-            },
-            child: const Text("Submid"),
+                child: const Text("POST"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var responseGet = await http.get(
+                    Uri.parse("https://reqres.in/api/users/2"),
+                  );
+                  // print(responseGet.body);
+                  Map<String, dynamic> dataResponGet =
+                      json.decode(responseGet.body) as Map<String, dynamic>;
+                  // print(dataRespon["name"]);
+                  setState(() {
+                    hasilResponse =
+                        "${dataResponGet['data']['first_name']} ${dataResponGet['data']['last_name']}- ${dataResponGet['data']['email']} [GET]";
+                  });
+                },
+                child: const Text("GET"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var responsePut = await http.put(
+                    Uri.parse("https://reqres.in/api/users/2"),
+                    body: {
+                      "name": namaC.text,
+                      "job": jobC.text,
+                    },
+                  );
+                  Map<String, dynamic> dataResponPut =
+                      json.decode(responsePut.body) as Map<String, dynamic>;
+                  // print(dataRespon["name"]);
+                  setState(() {
+                    hasilResponse =
+                        "${dataResponPut['name']} - ${dataResponPut['job']} [GET]";
+                  });
+                },
+                child: const Text("PUT"),
+              ),
+            ],
           ),
           const SizedBox(height: 50),
           const Divider(
@@ -70,7 +116,12 @@ class _HttpPostState extends State<HttpPost> {
           ),
           const SizedBox(height: 10),
           Center(
-            child: Text(hasilResponse),
+            child: Text(
+              hasilResponse,
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
           ),
         ],
       ),
